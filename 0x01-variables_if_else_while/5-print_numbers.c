c8ce7750e17dcf8f76cb84f01de3ced8e18e27db
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-18 00:35:28 +0100

    Print 0-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - A program that prints if the number is +ve, or -ve
+ * main - A program that prints numbers lesser than 10
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int n;
+	int num;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
-		printf("%d is zero\n", n);
-	}
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+		
+	printf("\n");
 
 	return (0);
 }
