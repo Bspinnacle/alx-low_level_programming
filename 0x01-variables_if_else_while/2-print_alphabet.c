5e3dacf8e96211ff83d4a726f887a4b18ee883a0
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-17 08:20:15 -0700

    Print last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints the last digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	i = n % 10;
+
+	if (i > 5)
+	{
+		printf("The last digit of %d is %d and is greater than 5\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("The last digit of %d is %d and is 0\n", i);
+	}
+	else
+	{
+		printf("The last digit of %d is %d and is less than 6 andnot 0\n", i);
+	}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints if the number is +ve, or -ve
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
