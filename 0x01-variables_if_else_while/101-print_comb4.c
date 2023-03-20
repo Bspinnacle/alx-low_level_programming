c8c385c7d1cd0f5acfba8368a29c7d5287e0ead5
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-20 13:24:08 -0700

    Combination of 3 different digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 9; a++)
 	{
 
 	for (b = a + 1; b <= 9; b++)
@@ -19,7 +19,7 @@ int main(void)
 
 	for (c = b + 1; c <= 9; c++)
 	{
-	if ((a != b) != c)
+	if ((b != a) != c)
 	{
 	putchar(a);
 	putchar(b);
