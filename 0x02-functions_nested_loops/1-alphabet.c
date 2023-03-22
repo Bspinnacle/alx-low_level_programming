64d4ef5f283cfebc5d07ff35bf5b27e844946ef7
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-21 23:53:04 -0700

    Lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A program that prints alphabets in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+	_putchar(alph);
+	}
+	_putchar('\n');
+}
