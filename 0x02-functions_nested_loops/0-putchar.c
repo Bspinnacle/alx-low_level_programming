280327f64672e35f13a77b3f308cbc1966e739dd
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-21 23:14:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_Putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_Putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
