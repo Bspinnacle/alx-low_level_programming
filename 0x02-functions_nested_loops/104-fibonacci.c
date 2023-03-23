5f6638db5ae2aa24b3fd59ebebbeb450faf0ad87
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-23 04:48:57 -0700

    4th fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 /**
- * main - a program that prints the first 52 numbers of fibonacci
- * Return: nothing
+ * main - a program that finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int a = 0;
-	long b = 1, c = 2;
+	int counter;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_h1, fib1_h2, fib2_h1, fib2_h2;
+	unsigned long h1, h2;
 
-	while (a < 50)
+	for (counter = 0; counter < 92; counter)
 	{
-	if (a  == 0)
+	sum = fib1 + fib2;
 	{
-	printf("%ld", b);
-	else if (a == 1)
-	printf(", %ld", c);
+	printf("%lu ", sum);
+	fib1 = fib2;
+	fib2 = sum;
 	}
-	else
+	fib1_h1 = fib1 / 10000000000;
+	fib1_h2 = fib1 % 10000000000;
+	fib2_h1 = fib2 / 10000000000;
+	fib2_h2 = fib2 % 10000000000;
+	for (counter = 93; counter < 99; counter++)
 	{
-	c += b;
-	b = c - b;
-	printf(", %ld", c);
+	h1 = fib1_h1 + fib2_h1;
+	h2 = fib1_h2 + fib2_h2;
+	if (fib1_h2 + fib2_h2 > 9999999999)
+	{
+	h1 += 1;
+	h2 %= 10000000000;
 	}
-	++a;
+	printf("%lu%lu", h1, h2);
+	if (counter != 98)
+	printf(", ");
+	fib1_h1 = fib2_h1;
+	fib1_h2 = fib2_h2;
+	fib2_h1 = h1;
+	fib2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
