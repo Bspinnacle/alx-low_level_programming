59864f994cedecea369046b525ac852293476aac
Bspinnacle <omoviesamuel@yahoo.com>
2023-03-26 00:19:25 -0700

    Multiplication)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - IT checks if a digit is between 0 to 9
+ * _isdigit - It checks if a digit is between 0 to 9
  * @c: input number
  * Return: 1 if c is a digit, 0 if otherwise
  */
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _isdigit - A program that checks for uppercase character
- * @c: character to be checked
- * Return: 1 if c is uppercase,0 if otherwise.
+ * mul - A program that multiplies two integers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ * Return: multiplication result
  */
-int _isupper(int c)
+int mul(int a, int b)
 {
-	if (c >= 65 && c <= 90)
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
+	return (a * b);
 }
