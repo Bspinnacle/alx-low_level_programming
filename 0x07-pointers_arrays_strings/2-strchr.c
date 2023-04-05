05acd9495991b68f86e231cf690a38c9069a7a44
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-05 12:03:03 -0700

    locate string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string
+ * @c: character
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; i++)
+	{
+	if (s[a] == c)
+	return (&s[a]);
+	}
+	return (0);
+}
