e06e4c93be300149f07425d57e97f4f9aaac41ad
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-20 14:48:25 -0700

    prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,24 +10,23 @@
  * Description: Full description
  */
 
-	struct dog
-        {
+struct dog
+{
 	char *name;
-	float age;777
+	float age;
 	char *owner;
-	};
+};
 
 /**
  * dog_t - typedef for struct dog
  */
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 typedef struct dog dog_t;
-	
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
