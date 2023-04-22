445c3b3e25560f3cf309f876097ccff927042efe
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-22 16:16:39 -0700

    multiply

--- 0x0C-more_malloc_free/102-mul.c
+++ 0x0C-more_malloc_free/102-mul.c
@@ -14,23 +14,23 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
+	if (argv[1][i] != '0')
+	{
+	isn1 = 0;
+	break;
+	}
 
 	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
+	if (argv[2][i] != '0')
+	{
+	isn2 = 0;
+	break;
+	}
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		printf("0\n");
-		exit(0);
+	printf("0\n");
+	exit(0);
 	}
 }
 
@@ -66,8 +66,8 @@ int _checknum(char *argv[], int n)
 	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
-			printf("Error\n");
-			exit(98);
+		printf("Error\n");
+		exit(98);
 		}
 
 	return (ln);
@@ -98,27 +98,27 @@ int main(int argc, char *argv[])
 	{
 		if (i < 0)
 		{
-			if (addl > 0)
+		if (addl > 0)
 			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
+			add = (nout[k] - '0') + addl;
+		if (add > 9)
+			nout[k - 1] = (add / 10) + '0';
+			nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			break;
+		lnout--;
+		free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+		k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
+		add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+		addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
