cf8cfcb60d30a896b1059fb5ff65b0e81f77f27b
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-22 16:08:28 -0700

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.cc
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat -  a function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: amount of bytes
+ *
+ * Return: pointer
+ * if malloc fails, NUll is returned
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int a, b, c, i;
+
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+	for (a = 0; s1[a] != '\0'; a++)
+	;
+	for (b = 0; s2[b] != '\0'; b++)
+	;
+	if (n > b)
+	n = b;
+	c = a + n;
+	s = malloc(c + 1);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < c; i++)
+	if (i < a)
+	s[i] = s1[i];
+	else
+	s[i] = s2[i - a];
+	s[i] = '\0';
+	return (s);
+}
