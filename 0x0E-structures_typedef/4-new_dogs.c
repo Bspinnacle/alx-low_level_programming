52c608172fe4232dbbd6f37471f963f2e2175e00
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-22 15:11:24 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dogs.c
+++ 0x0E-structures_typedef/4-new_dogs.c
@@ -13,47 +13,41 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
-
 	while (s[i] != '\0')
 	{
-		i++;
+	i++;
 	}
-
 	return (i);
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
+ *_strcpy - A function that copies the string pointed to by src including
+ * the terminating null byte (\0) to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
  * Return: the pointer to dest
  */
 
-char *_strcpy(char *dest, char *src)
+	char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int n, i;
 
-	len = 0;
+	n = 0;
 
-	while (src[len] != '\0')
+	while (src[n] != '\0')
 	{
-		len++;
+	++;
 	}
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
 
 /**
- * new_dog - creates a new dog
+ * new_dog - A function that creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -63,6 +57,7 @@ char *_strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+
 	dog_t *dog;
 	int len1, len2;
 
@@ -76,18 +71,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
-		free(dog);
-		return (NULL);
+	free(dog);
+	return (NULL);
 	}
+
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
-		free(dog);
-		free(dog->name);
-		return (NULL);
+	free(dog);
+	free(dog->name);
+	return (NULL);
 	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
+
 	dog->age = age;
 
 	return (dog);
