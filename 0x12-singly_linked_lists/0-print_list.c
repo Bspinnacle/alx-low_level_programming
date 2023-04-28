086168363b34290df0157f5cca68853d4e642cf4
Bspinnacle <omoviesamuel@yahoo.com>
2023-04-28 08:02:07 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
@@ -21,8 +21,8 @@ size_t print_list(const list_t *h)
 		else
 		printf("[%d]%s\n", h->len, h->str);
 		h = h->next;
-		node++;
+		nodes++;
 	}
 
-		return (node);
+		return (nodes);
 }
