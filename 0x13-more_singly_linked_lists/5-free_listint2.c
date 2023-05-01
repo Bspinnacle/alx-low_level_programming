7052caf7406e23eb880dc9527b397b03ea8db1b6
Bspinnacle <omoviesamuel@yahoo.com>
2023-05-01 12:28:27 -0700

    free listint lists2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head: The beginning of list
+ *
+ * Return: no return
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+}
