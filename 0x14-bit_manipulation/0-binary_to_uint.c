73179cc53f60a19a193847bb45de90a9f5db4ae7
Bspinnacle <omoviesamuel@yahoo.com>
2023-05-05 04:27:06 -0700

    convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,8 +7,9 @@
  * to an unsigned int
  * @b: binary number
  *
- * return: the converted number, or 0 if there is one or more chars
- * in the string b that is not 0 or 1
+ * return: the converted number,
+ * or 0 if there is one or more chars in the string b that is not 0
+ * or 1 if b is NULL
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -23,8 +24,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < 0 || b[i] > 1)
+		if (b[i] < '0' || b[i] > '1')
 		return (0);
+		uint = 2 * uint + (b[i] - '0');
 	}
 
 	return (uint);
