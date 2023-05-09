1f496d33ddb5b049a25078424922627021224875
Bspinnacle <omoviesamuel@yahoo.com>
2023-05-09 12:12:42 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,11 +24,11 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	}
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT|O_RDWR|O_TRUNC, 0600);
 	n = write(fp, text_content, length);
 
 	if (fp == -1 || n == -1)
-		return (-1);
+	return (-1);
 
 	close(fp);
 	return (1);
