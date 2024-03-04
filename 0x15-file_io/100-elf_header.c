45d5e45d606ca43671c96bdab4b8c1e5c942ccf7
Bspinnacle <omoviesamuel@yahoo.com>
2024-03-04 12:06:51 -0800

    elf correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -42,7 +42,7 @@ void print_magic(unsigned char *e_ident)
 {
 	int id;
 
-	printf("Magic: ");
+	printf("Magic: 		");
 
 	for (id = 0; id < EI_NIDENT; id++)
 	{
@@ -106,7 +106,7 @@ void print_data(unsigned char *e_ident)
 		break;
 
 		case ELFDATA2LSB:
-		printf("2's Complement, little endian\n");
+		printf("2's complement, little endian\n");
 		break;
 
 		case ELFDATA2MSB:
